b14047c9f52224bb963b0cd4eb9a398c607ca416
“krisschaafaclue” <kristoffer.schaaf@aclue.de>
2023-05-21 15:31:48 +0200

    implement content range

--- src/main/java/de/hawhamburg/ti/inf/rnp/HttpGet.java
+++ src/main/java/de/hawhamburg/ti/inf/rnp/HttpGet.java
@@ -7,7 +7,12 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class HttpGet {
-    HttpGet(String remoteHost, int port, String file){
+
+    HttpGet(WebServer webServer){
+        String remoteHost = webServer.getHost();
+        int port = webServer.getPort();
+        String file = webServer.getFile();
+
         try {
             Socket s = new Socket(remoteHost, port);
 
@@ -20,10 +25,38 @@ public class HttpGet {
             BufferedReader bufRead = new BufferedReader(new InputStreamReader(s.getInputStream()));
             String outStr;
 
-            while((outStr = bufRead.readLine()) != null) {
-                System.out.println(outStr);
-            }
+            if(webServer.getContentRangeStart() == -1) {
+                //The whole Data will be passed
+                while((outStr = bufRead.readLine()) != null) {
+                    System.out.println(outStr);
+                }
+            } else if (webServer.getContentRangeStart() != 1 && webServer.getContentRangeEnd() == -1) {
+                // Data from given Content-Range start will be passed
+                int passedDataInBytes = 0;
 
+                while((outStr = bufRead.readLine()) != null) { //TODO read by byte and not by line
+                    passedDataInBytes += outStr.getBytes().length;
+                    if(passedDataInBytes > webServer.getContentRangeStart()) {
+                        System.out.println(outStr);
+                    }
+                }
+            } else if (webServer.getContentRangeStart() != 1 && webServer.getContentRangeEnd() != -1) {
+                // Data in between given Content-Range start and Content-Range end will be passed
+                int passedDataInBytes = 0;
+
+                while((outStr = bufRead.readLine()) != null) { //TODO read by byte and not by line
+                    passedDataInBytes += outStr.getBytes().length;
+                    if(passedDataInBytes > webServer.getContentRangeStart()) {
+                        System.out.println(outStr);
+                    }
+                    if(passedDataInBytes > webServer.getContentRangeEnd()) {
+                        break;
+                    }
+                }
+            } else {
+                System.err.println("No valid Content-Range.");
+                System.exit(-1);
+            }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
--- src/main/java/de/hawhamburg/ti/inf/rnp/WebServer.java
+++ src/main/java/de/hawhamburg/ti/inf/rnp/WebServer.java
@@ -2,37 +2,85 @@ package de.hawhamburg.ti.inf.rnp;
 
 import picocli.CommandLine;
 
-public class WebServer {
+import static picocli.CommandLine.*;
+
+@Command(name = "WebServer", mixinStandardHelpOptions = true, version = "v3.0.0")
+public class WebServer implements Runnable{
+
+    @Option(names = { "-h", "--host" }, description = "Host")
+    private String host;
+
+    @Option(names = { "-p", "--port" }, description = "Port")
+    private int port = 80;
+
+    @Option(names = { "-f", "--file" }, description = "File")
+    private String file;
+
+    @Option(names = { "-r", "--range" }, description = "Content-Range")
+    private String contentRange = "-1";
+    @Option(names = { "-s", "--slow"}, description = "Slow-Motion")
+    private int[] slowMotion = {-1,-1};
+
+    @Override
+    public void run() {
+        if(this.getPort()<=0 || this.getPort() > 65535) {
+            System.err.println("Port must be between 1 and 65535");
+            System.exit(-1);
+        }
+        if(this.getFile().isEmpty()) {
+            System.err.println("Path cannot be empty");
+            System.exit(-1);
+        }
+
+        System.out.println("Getting " + this.getFile() + " from " + this.getHost() + ":" + this.getPort());
+
+        HttpGet hg = new HttpGet(this);
+    }
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new WebServer()).execute(args); // |7|
+        System.exit(exitCode); // |8|
+    }
 
-        System.out.println("Argument count: " + args.length);
+    public String getHost() {
+        return this.host;
+    }
 
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("Argument " + i + ": " + args[i]);
+    public int getPort() {
+        return this.port;
+    }
+
+    public String getFile() {
+        return this.file;
+    }
+
+    private boolean contentRangeContainsEnd() {
+        return this.contentRange.matches("^[0-9]+-[0-9]+$");
+    }
+
+    public int getContentRangeStart() {
+        if (contentRangeContainsEnd()) {
+            String[] contentRanges = this.contentRange.split("-");
+            return Integer.parseInt(contentRanges[0]);
+        } else {
+            return Integer.parseInt(contentRange);
         }
+    }
 
-        ArgumentDecryptor argumentDecryptor = new ArgumentDecryptor();
-
-        try {
-            CommandLine.ParseResult parseResult = new CommandLine(argumentDecryptor).parseArgs(args);
-            if (!CommandLine.printHelpIfRequested(parseResult)) {
-
-                if(argumentDecryptor.getPort()<=0 || argumentDecryptor.getPort() > 65535) {
-                    System.err.println("Port must be between 1 and 65535");
-                    System.exit(-1);
-                }
-                if(argumentDecryptor.getFile().isEmpty()) {
-                    System.err.println("Path cannot be empty");
-                    System.exit(-1);
-                }
-
-                System.out.println("Getting " + argumentDecryptor.getFile() + " from " + argumentDecryptor.getHost() + ":" + argumentDecryptor.getPort());
-
-                HttpGet hg = new HttpGet(argumentDecryptor.getHost(), argumentDecryptor.getPort(), argumentDecryptor.getFile());
-            }
-        } catch (CommandLine.ParameterException ex) {
-            System.err.println(ex.getMessage());
-            ex.getCommandLine().usage(System.err);
+    public int getContentRangeEnd() {
+        if (contentRangeContainsEnd()) {
+            String[] contentRanges = this.contentRange.split("-");
+            return Integer.parseInt(contentRanges[1]);
+        } else {
+            return -1;
         }
+    }
+
+    public int getSlowMoBytes() {
+        return this.slowMotion[0];
+    }
+
+    public int getSlowMoTime() {
+        return this.slowMotion[1];
     }
 }
