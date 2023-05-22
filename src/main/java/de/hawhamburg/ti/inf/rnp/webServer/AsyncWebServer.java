e50028e651f7568e33cb30ab366e02b1e36787e7
“krisschaafaclue” <kristoffer.schaaf@aclue.de>
2023-05-22 09:38:03 +0200

    make webserver async

--- src/main/java/de/hawhamburg/ti/inf/rnp/webServer/AsyncWebServer.java
+++ src/main/java/de/hawhamburg/ti/inf/rnp/webServer/AsyncWebServer.java
@@ -1,6 +1,7 @@
 package de.hawhamburg.ti.inf.rnp.webServer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -15,10 +16,14 @@ public class AsyncWebServer implements Runnable {
 
     @Override
     public void run() {
-        this.start();
+        try {
+            this.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void start() {
+    private void start() throws IOException {
         ServerSocket s;
 
         System.out.println("Webserver starting up on port 80");
@@ -33,32 +38,12 @@ public class AsyncWebServer implements Runnable {
         System.out.println("Waiting for connection");
 
         while (true) {
-            try {
-                Socket remote = s.accept();
+            Socket remote = s.accept();
 
-                System.out.println("Connection, sending data.");
+            Responder responder = new Responder(remote);
+            responder.run();
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        remote.getInputStream()));
-
-                PrintWriter out = new PrintWriter(remote.getOutputStream());
-
-                String str = ".";
-
-                while (!str.equals(""))
-                    str = in.readLine();
-
-                out.println("HTTP/1.0 200 OK");
-                out.println("Content-Type: text/html");
-                out.println("Server: RNP WebServer");
-                out.println(WebServerUtils.CONTENT_HEADER);
-                out.println(WebServerUtils.CONTENT_PARAGRAPH);
-                out.flush();
-
-                remote.close();
-            } catch (Exception e) {
-                System.out.println("Error: " + e);
-            }
+            System.out.println("Connection, sending data.");
         }
     }
 }
--- src/main/java/de/hawhamburg/ti/inf/rnp/webServer/Responder.java
+++ src/main/java/de/hawhamburg/ti/inf/rnp/webServer/Responder.java
@@ -1,8 +1,40 @@
 package de.hawhamburg.ti.inf.rnp.webServer;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Responder implements Runnable {
+    private Socket remote;
+
+    public Responder(Socket socket) {
+        this.remote = socket;
+    }
+
     @Override
     public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    remote.getInputStream()));
 
+            PrintWriter out = new PrintWriter(remote.getOutputStream());
+
+            String str = ".";
+
+            while (!str.equals(""))
+                str = in.readLine();
+
+            out.println("HTTP/1.0 200 OK");
+            out.println("Content-Type: text/html");
+            out.println("Server: RNP WebServer");
+            out.println(WebServerUtils.CONTENT_HEADER);
+            out.println(WebServerUtils.CONTENT_PARAGRAPH);
+            out.flush();
+
+            remote.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
